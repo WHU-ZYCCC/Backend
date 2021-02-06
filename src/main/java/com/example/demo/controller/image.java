956243b863d3545a366476281140be052714c6c2
A-Salty-Fish <1309032417@qq.com>
2021-02-06 13:20:29 +0800

    添加COS依赖

--- src/main/java/com/example/demo/controller/image.java
+++ src/main/java/com/example/demo/controller/image.java
@@ -1,5 +1,10 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * @author dzy
  * @title: image
@@ -7,5 +12,12 @@ package com.example.demo.controller;
  * @description: TODO
  * @date 2021/2/613:13
  */
+@Controller
+@RequestMapping(value = "/image")
+@ResponseBody
 public class image {
+    @GetMapping("/test")
+    public int test() {
+        return 0;
+    }
 }
