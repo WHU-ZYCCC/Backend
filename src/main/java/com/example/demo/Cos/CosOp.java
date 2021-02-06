405d66fa0e791244c2b3b16815ddfe3a0d4d50fb
A-Salty-Fish <1309032417@qq.com>
2021-02-06 13:39:35 +0800

    列出Key

---
+++ src/main/java/com/example/demo/Cos/CosOp.java
@@ -0,0 +1,63 @@
+package com.example.demo.Cos;
+
+import com.qcloud.cos.COSClient;
+import com.qcloud.cos.exception.CosClientException;
+import com.qcloud.cos.exception.CosServiceException;
+import com.qcloud.cos.model.COSObjectSummary;
+import com.qcloud.cos.model.ListObjectsRequest;
+import com.qcloud.cos.model.ObjectListing;
+
+import java.util.List;
+
+/**
+ * @author dzy
+ * @title: CosOp
+ * @projectName demo
+ * @description: TODO
+ * @date 2021/2/613:30
+ */
+public class CosOp {
+    public static void testSearch() {
+        COSClient cosClient = CosConfig.GetCosClient();
+        // Bucket的命名格式为 BucketName-APPID ，此处填写的存储桶名称必须为此格式
+        String bucketName = "picture-1304825952";
+        ListObjectsRequest listObjectsRequest = new ListObjectsRequest();
+// 设置bucket名称
+        listObjectsRequest.setBucketName(bucketName);
+// prefix表示列出的object的key以prefix开始
+        listObjectsRequest.setPrefix("");
+// deliter表示分隔符, 设置为/表示列出当前目录下的object, 设置为空表示列出所有的object
+        listObjectsRequest.setDelimiter("/");
+// 设置最大遍历出多少个对象, 一次listobject最大支持1000
+        listObjectsRequest.setMaxKeys(1000);
+        ObjectListing objectListing = null;
+        do {
+            try {
+                objectListing = cosClient.listObjects(listObjectsRequest);
+            } catch (CosServiceException e) {
+                e.printStackTrace();
+                return;
+            } catch (CosClientException e) {
+                e.printStackTrace();
+                return;
+            }
+            // common prefix表示表示被delimiter截断的路径, 如delimter设置为/, common prefix则表示所有子目录的路径
+            List<String> commonPrefixs = objectListing.getCommonPrefixes();
+            // object summary表示所有列出的object列表
+            List<COSObjectSummary> cosObjectSummaries = objectListing.getObjectSummaries();
+            for (COSObjectSummary cosObjectSummary : cosObjectSummaries) {
+                // 文件的路径key
+                String key = cosObjectSummary.getKey();
+                // 文件的etag
+                String etag = cosObjectSummary.getETag();
+                // 文件的长度
+                long fileSize = cosObjectSummary.getSize();
+                // 文件的存储类型
+                String storageClasses = cosObjectSummary.getStorageClass();
+                System.out.println(key);
+            }
+            String nextMarker = objectListing.getNextMarker();
+            listObjectsRequest.setMarker(nextMarker);
+        } while (objectListing.isTruncated());
+    }
+}
