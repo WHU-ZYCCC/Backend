260999457f6c3758493fffd2b470dfc266640207
A-Salty-Fish <1309032417@qq.com>
2021-02-20 20:55:33 +0800

    处理图像标签返回值

--- src/main/java/com/example/demo/Img/ImgOp.java
+++ src/main/java/com/example/demo/Img/ImgOp.java
@@ -1,12 +1,18 @@
 package com.example.demo.Img;
 
+import com.alibaba.fastjson.JSON;
 import com.tencentcloudapi.common.Credential;
 import com.tencentcloudapi.common.exception.TencentCloudSDKException;
 import com.tencentcloudapi.common.profile.ClientProfile;
 import com.tencentcloudapi.common.profile.HttpProfile;
 import com.tencentcloudapi.tiia.v20190529.TiiaClient;
+import com.tencentcloudapi.tiia.v20190529.models.DetectLabelItem;
 import com.tencentcloudapi.tiia.v20190529.models.DetectLabelRequest;
 import com.tencentcloudapi.tiia.v20190529.models.DetectLabelResponse;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author dzy
@@ -27,10 +33,23 @@ public class ImgOp {
             DetectLabelRequest req = new DetectLabelRequest();
             req.setImageUrl(imgUrl);
             DetectLabelResponse resp = client.DetectLabel(req);
-            return DetectLabelResponse.toJsonString(resp);
+            return JSON.toJSONString(handleLabels(resp));
         } catch (TencentCloudSDKException e) {
             System.out.println(e.toString());
             return "";
         }
+    }
+
+    /**
+     * 处理返回标签的函数
+     * @param resp
+     * @return
+     */
+    public static List<String> handleLabels(DetectLabelResponse resp) {
+        List<String> list = new LinkedList<>();
+        Arrays.stream(resp.getLabels()).forEach(di -> {
+            list.add(di.getName());
+        });
+        return list;
     }
 }
