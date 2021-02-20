c929bfeaf776afacc79518c674e493ce11ab15a7
A-Salty-Fish <1309032417@qq.com>
2021-02-20 20:27:00 +0800

    初始化图像识别返回值函数

--- src/main/java/com/example/demo/Img/ImgOp.java
+++ src/main/java/com/example/demo/Img/ImgOp.java
@@ -1,5 +1,13 @@
 package com.example.demo.Img;
 
+import com.tencentcloudapi.common.Credential;
+import com.tencentcloudapi.common.exception.TencentCloudSDKException;
+import com.tencentcloudapi.common.profile.ClientProfile;
+import com.tencentcloudapi.common.profile.HttpProfile;
+import com.tencentcloudapi.tiia.v20190529.TiiaClient;
+import com.tencentcloudapi.tiia.v20190529.models.DetectLabelRequest;
+import com.tencentcloudapi.tiia.v20190529.models.DetectLabelResponse;
+
 /**
  * @author dzy
  * @title: ImgOp
@@ -8,4 +16,21 @@ package com.example.demo.Img;
  * @date 2021/2/2020:23
  */
 public class ImgOp {
+    public static String GetImgLable(String imgUrl) {
+        try{
+            Credential cred = new Credential("11", "");
+            HttpProfile httpProfile = new HttpProfile();
+            httpProfile.setEndpoint("tiia.tencentcloudapi.com");
+            ClientProfile clientProfile = new ClientProfile();
+            clientProfile.setHttpProfile(httpProfile);
+            TiiaClient client = new TiiaClient(cred, "ap-shanghai", clientProfile);
+            DetectLabelRequest req = new DetectLabelRequest();
+            req.setImageUrl("11111111");
+            DetectLabelResponse resp = client.DetectLabel(req);
+            return resp.toString();
+        } catch (TencentCloudSDKException e) {
+            System.out.println(e.toString());
+            return "";
+        }
+    }
 }
